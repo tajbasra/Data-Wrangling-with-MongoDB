f29cc69f7fbc2254bc4aed0504c1bddef364b095
T Basra <tajinderkbasra@Gmail.com>
2016-04-25 09:33:09 +0100

    Added files via upload

---
+++ lesson1ex1.py
@@ -0,0 +1,45 @@
+import csv
+import os
+
+DATADIR = ""
+DATAFILE = "745090.csv"
+
+
+def parse_file(datafile):
+    name = ""
+    data = []
+    with open(datafile,'rb') as f:
+        nameline = f.readline().split(",")
+        name = nameline[1].strip('"')
+        header = next(f).split(",")
+        for line in f: 
+                entry = line.split(',')
+                data.append(entry)
+    # Do not change the line below
+    return (name, data)
+
+# the above is my code. below is the solution code. much simpler!
+'''
+def parse_file(datafile):
+    name = ""
+    data = []
+    with open(datafile,'rb') as f:
+        r = csv.reader(f)
+        name = r.next()[1]
+        header = r.next()
+        data = [row for row in r]
+
+    return (name, data)
+'''
+def test():
+    datafile = os.path.join(DATADIR, DATAFILE)
+    name, data = parse_file(datafile)
+
+    assert name == "MOUNTAIN VIEW MOFFETT FLD NAS"
+    assert data[0][1] == "01:00"
+    assert data[2][0] == "01/01/2005"
+    assert data[2][5] == "2"
+
+
+if __name__ == "__main__":
+    test()
